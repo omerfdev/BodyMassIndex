a5e924d09f0d746de813a622bee3081cc45fdaeb
omeralmali1 <omeralmali1@gmail.com>
2021-02-28 13:31:51 +0300

    Add project files.

---
+++ BodyMassIndex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BodyMassIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Double height, weigth, bmi;
+            Console.WriteLine("Welcome To Body Mass Index");
+            Console.WriteLine("Please Enter Your Height");
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter Your Weigth");
+            weigth = Convert.ToDouble(Console.ReadLine());
+            bmi = weigth / (height*height);
+
+
+        }
+    }
+}
